9a74ba4a6b3469af4e1860a1fcf1393afb92de0e
NadyaCodes <nadyacorscadden@gmail.com>
2023-01-24 19:41:20 -0500

    fetch dog breeds

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [breeds, setBreeds] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/list/all")
+      .then((response) => response.json())
+      .then((data) => {
+        const allBreedNames = Object.keys(data.message);
+        setBreeds(allBreedNames);
+      });
+  });
+
+  const breedsDisplay = breeds.map((breed, index) => {
+    return <div key={index}>{breed}</div>;
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{breedsDisplay}</div>
     </div>
   );
 }
